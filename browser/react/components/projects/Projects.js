aececdb84c5142180f147283b259e8be17e15bd8
Jake Peyser <jakepeyser@gmail.com>
2016-10-28 15:59:32 -0400

    Added project switcher animation

--- browser/react/components/projects/Projects.js
+++ browser/react/components/projects/Projects.js
@@ -2,25 +2,31 @@ import React from'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router';
 
-export default ({ projects, fullView, children }) => (
-  <div id="portfolio-wrapper">
-    <Helmet title="Portfolio" />
-    <div id={fullView ? 'portfolio' : 'portfolio-switcher'}>
-    {
-      projects.map(project => {
-        return (
-          <div key={project.id} className={fullView ? 'project' : 'project-switcher'}>
-            <Link to={`/projects/${project.filename}`}
-            className={`project-bubble i-${project.filename}`} activeClassName="active-bubble">
-              <div className="project-text-wrapper">
-                <span>{project.name}</span>
-              </div>
-            </Link>
-          </div>
-        )
-      })
-    }
+export default ({ projects, fullView, children }) => {
+  const size = fullView ? 'lg' : 'sm';
+  return (
+    <div id="portfolio-wrapper">
+      <Helmet title="Portfolio" />
+      <div className={`portfolio portfolio-${size}`}>
+      {
+        projects.map(project => {
+          return (
+            <div key={project.id} className={`project project-${size}`}>
+              <Link to={`/projects/${project.filename}`}
+                className={`project-bubble project-bubble-${size} i-${project.filename}`}
+                activeClassName="active-bubble">
+                <div className={"project-text-wrapper"}>
+                  <span className={`project-bubble-text project-bubble-text-${size}`}>
+                    { project.name }
+                  </span>
+                </div>
+              </Link>
+            </div>
+          )
+        })
+      }
+      </div>
+      { children }
     </div>
-    { children }
-  </div>
-);
+  )
+};
