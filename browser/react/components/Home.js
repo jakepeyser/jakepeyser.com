33a73c2be945fa731371b300e3d2973f949cd51d
Jake Peyser <jakepeyser@gmail.com>
2016-12-14 10:32:01 -0500

    added home page wallpaper

--- browser/react/components/Home.js
+++ browser/react/components/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default () => (
   <div id="home">
-    <div className="info">
-      <span className="name">Jake Peyser</span>
-      <span className="title">software engineer</span>
+    <div className="wallpaper"></div>
+    <div className="info-wrapper">
+      <div className="info">
+        <span className="name">Jake Peyser</span>
+        <span className="title">full stack developer</span>
+      </div>
     </div>
   </div>
 );
