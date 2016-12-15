70363ee55464c04b8308589fced54ba7da8fea78
Jake Peyser <jakepeyser@gmail.com>
2016-12-15 18:10:45 -0500

    changed pdf viewer to pdfjs and hosting myself

--- browser/react/components/Resume.js
+++ browser/react/components/Resume.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-
-// Embedded Google doc located here:
-// https://drive.google.com/file/d/0B74g2zDubN9jcmFXV3lxWEM4RVE/view?usp=sharing
-const resumeDownLoadLink =
-  <a download href="https://drive.google.com/uc?export=download&id=0B74g2zDubN9jcmFXV3lxWEM4RVE">download</a>
+import resume from '../../src/resume.pdf';
+const resumeFilePath = '/browser/src/resume.pdf';
 
 export default () => (
   <div id="resume">
@@ -12,13 +9,12 @@ export default () => (
     <div className="resume-download">
       <p>
         If you are having trouble viewing the resume, you can
-        &nbsp;{ resumeDownLoadLink }&nbsp;
+        &nbsp;{ <a download href={ resumeFilePath }>download</a> }&nbsp;
         a copy
       </p>
     </div>
     <iframe
-      id="resume"
-      src="https://docs.google.com/viewer?srcid=0B74g2zDubN9jcmFXV3lxWEM4RVE&pid=explorer&embedded=true" >
-    </iframe>
+      src={`http://mozilla.github.io/pdf.js/web/viewer.html?file=https://jakepeyser-test.herokuapp.com${resumeFilePath}`}
+    />
   </div>
 );
--- server/app.js
+++ server/app.js
@@ -21,6 +21,11 @@ app.use((req, res, next) => {
 
 // Logging, static, and body-parser middleware
 app.use(morgan('dev'));
+app.use('*.pdf', function(req, res, next) { // allow CORS for PDF requests
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.static(PATHS.build));
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
