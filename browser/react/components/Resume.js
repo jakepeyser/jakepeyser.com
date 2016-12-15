abb74c3d7464d9769c43cfffebab3620dfd04b30
Jake Peyser <jakepeyser@gmail.com>
2016-12-15 18:19:46 -0500

    changed resume pdf url to prod

--- browser/react/components/Resume.js
+++ browser/react/components/Resume.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import resume from '../../src/resume.pdf';
-const resumeFilePath = '/browser/src/resume.pdf';
 
 export default () => (
   <div id="resume">
@@ -9,12 +8,12 @@ export default () => (
     <div className="resume-download">
       <p>
         If you are having trouble viewing the resume, you can
-        &nbsp;{ <a download href={ resumeFilePath }>download</a> }&nbsp;
+        &nbsp;{ <a download href={ resume }>download</a> }&nbsp;
         a copy
       </p>
     </div>
     <iframe
-      src={`http://mozilla.github.io/pdf.js/web/viewer.html?file=https://jakepeyser-test.herokuapp.com${resumeFilePath}`}
+      src={`http://mozilla.github.io/pdf.js/web/viewer.html?file=http://jakepeyser.com/${resume}`}
     />
   </div>
 );
