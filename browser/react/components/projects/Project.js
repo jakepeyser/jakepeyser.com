5e55ee67a5b718ee6be4df6cae038f4f072dfbf3
Jake Peyser <jakepeyser@gmail.com>
2017-01-06 00:57:53 -0500

    Parallax scrolling (#31)
    * parallax scrolling for project page banners - closes #12
    
    * mobile, landscape view now has smaller project bubbles and associated styling

--- browser/react/components/projects/Project.js
+++ browser/react/components/projects/Project.js
@@ -4,55 +4,62 @@ import Helmet from 'react-helmet';
 export default ({ project }) => (
   <div id="project">
     <Helmet title={ project.name } />
-    <div className={`project-banner bg-${project.filename}`}>
-      <div className="project-title-container">
-        <h1 className="name">{project.name}</h1>
-        <div className="links">
-        {
-          project.links && project.links.map((link, idx) => {
-            return (
-              <a key={idx} href={ link.url } target="_blank">
-                <i className={`profile-icon fa fa-${link.type} fa-2x`}
-                aria-hidden="true"></i>
-              </a>
-            )
-          })
-        }
-        </div>
-      </div>
+    <div className="project-banner">
+      <div className={`bg-${project.filename}`} />
     </div>
-    <div className="sections">
-      <div className="overview">
-        <div className="text">
-          <h2>App Overview</h2>
-          <p>{ project.summary }</p>
-        </div>
-        <div className="screenshot">
-          <div className="img-wrapper">
-            <img src={`/browser/src/images/${project.filename}/screenshot.png`}
-              alt={ `${project.name} Screenshot` }/>
-          </div>
-        </div>
-      </div>
-      <div className="tech">
-        <div className="text">
-          <h2>Tech</h2>
-          <ul>
+    <div className="project-content">
+      <div className="project-title-wrapper">
+        <div className="project-title-container">
+          <h1 className="name">{project.name}</h1>
+          <div className="links">
           {
-            project.tech && project.tech.map(tech => {
+            project.links && project.links.map((link, idx) => {
               return (
-                <li key={tech.id}>
-                  <a href={tech.link} target="_blank">{tech.name}</a>
-                </li>
+                <a key={idx} href={ link.url } target="_blank">
+                  <i className={`profile-icon fa fa-${link.type} fa-2x`}
+                  aria-hidden="true"></i>
+                </a>
               )
             })
           }
-          </ul>
+          </div>
         </div>
-        <div className="screenshot">
-          <div className="img-wrapper">
-            <img src={`/browser/src/images/${project.filename}/mobile.png`}
-            alt={ `${project.name} Mobile View` }/>
+      </div>
+      <div className="sections-wrapper">
+        <div className="sections">
+          <div className="overview">
+            <div className="text">
+              <h2>App Overview</h2>
+              <p>{ project.summary }</p>
+            </div>
+            <div className="screenshot">
+              <div className="img-wrapper">
+                <img src={`/browser/src/images/${project.filename}/screenshot.png`}
+                  alt={ `${project.name} Screenshot` }/>
+              </div>
+            </div>
+          </div>
+          <div className="tech">
+            <div className="text">
+              <h2>Tech</h2>
+              <ul>
+              {
+                project.tech && project.tech.map(tech => {
+                  return (
+                    <li key={tech.id}>
+                      <a href={tech.link} target="_blank">{tech.name}</a>
+                    </li>
+                  )
+                })
+              }
+              </ul>
+            </div>
+            <div className="screenshot">
+              <div className="img-wrapper">
+                <img src={`/browser/src/images/${project.filename}/mobile.png`}
+                alt={ `${project.name} Mobile View` }/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
