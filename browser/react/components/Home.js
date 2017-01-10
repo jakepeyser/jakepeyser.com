jakepeyser/jakepeyser.com

commit f8d2e8240e1ee11264d7ca61e1821c6d70b87f20

fix #26 regression (#32)

browser/react/components/projects/Projects.js
@@ -14,7 +14,7 @@ export default ({ projects, fullView, children }) => {
           return (
             <Link key={project.id}
               to={`/projects/${project.filename}`}
-              onClick={ () => document.body.scrollTop = 0 }
+              onClick={ () => document.querySelector('#project').scrollTop = 0 }
               className={`project-bubble project-bubble-${size}`}
               activeClassName="active-bubble">
               <div className="project-background">

commit a628536cee3b568ab51c71f3ee1a39486e8e6be6

Subtle animations (#33)
- basic animations
- fixes page reload issue caused by https://github.com/jakepeyser/jakepeyser.com/commit/f8d2e8240e1ee11264d7ca61e1821c6d70b87f20

.travis.yml
@@ -14,4 +14,4 @@ deploy:
     secure: tZlBNXorEP4H1qbIgieOmcnMZ2OU71iithwDGk4B/Ozj6eocxkv/ijrUo1p4+Y69Q0JB8bqEAyxPW3DOKkOSD4NmkZomFHtgWy9vMRUZmGk4JekaEGv3ugUB31voaKZsMyk5cG8MBJyqI2Co2N4jQQyagb5IxzC7wEvMqoUU0VsO2CJ13veNu8JH3Y3E71geE0je7sLljG8UkYQCxDf8Zt5PPb8O2DWCW+M5YzG15Ql26Qc0thrF9eDBFiorinenRqlVBoJSCCr0BOeqmMCpUzPg7upNHAQp/A7dCz7bMxLvd6xf+BWAggPthOEfAnAAdOLusrJn9MsfcQWsxW6yGk35Cj9tQW/MHWsSPQ1sHnceFrRC1Zl4sJUbFHXv+xwXL8u5luFIK0RhCr50/dRPyZS2BWzbY6JUkr9eKuSYsAVIRwdTv7Q4FQegVBpR7JEKeZOTl3CbCeRQEIQjjhKu73wvygAKIENKqWUA+lGaZ8YQzjc1k/6oPUKGSuzNcdg8sqsXel//Du9Xu95mNL9uxboxl9iFQg8v7jnK0Jx647pqxvjXLNdr/jQ7Ty6DuOwqZrEBWCgTk37lHfv8C1bZwz7LDxX7TEDPW9rUq2ksidQu8pt/XbisctO0Y/X4yLLj/PG/AT0zLA+b2ZOBj7T1YfqXRTj+MkW4/7bl7PowiT4=
   app:
     master: jakepeyser
-    parallax: jakepeyser-test
+    home_animations: jakepeyser-test

browser/react/components/Home.js
@@ -4,7 +4,7 @@ export default () => (
   <div id="home">
     <div className="wallpaper"></div>
     <div className="info-wrapper">
-      <div className="info">
+      <div className="info animated fadeInDown">
         <span className="name">Jake Peyser</span>
         <span className="title">full stack developer</span>
       </div>

browser/react/components/contact/Contact.js
@@ -8,7 +8,7 @@ export default (props) => {
   let formDiv;
   if (Object.keys(props.messageStatus).length) {
     formDiv =
-      <div className="form-sent">
+      <div className="form-sent animated flipInY">
         <p>{`Thank you for your message, ${props.name}. ${props.messageStatus.message} `}</p>
         {
           props.messageStatus.success ?

browser/react/components/projects/Projects.js
@@ -12,22 +12,24 @@ export default ({ projects, fullView, children }) => {
       {
         projects.map(project => {
           return (
-            <Link key={project.id}
-              to={`/projects/${project.filename}`}
+            <div key={project.id}
               onClick={ () => document.querySelector('#project').scrollTop = 0 }
-              className={`project-bubble project-bubble-${size}`}
-              activeClassName="active-bubble">
-              <div className="project-background">
-                <div className={'project-wrapper'}>
-                  <div className={`project-icon i-${project.filename}`} />
-                  <div className="project-text-wrapper">
-                    <span className={`project-bubble-text project-bubble-text-${size}`}>
-                      { project.name }
-                    </span>
+              className={`${fullView ? 'animated rollIn' : ''}`}>
+              <Link to={`/projects/${project.filename}`}
+                className={`project-bubble project-bubble-${size}`}
+                activeClassName="active-bubble">
+                <div className="project-background">
+                  <div className={'project-wrapper'}>
+                    <div className={`project-icon i-${project.filename}`} />
+                    <div className="project-text-wrapper">
+                      <span className={`project-bubble-text project-bubble-text-${size}`}>
+                        { project.name }
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           )
         })
       }

browser/src/stylesheets/project.scss
@@ -8,7 +8,7 @@
   // Lock perspective and render children relative to fixed
   perspective: 1px;
   overflow-x: hidden;
-  overflow-y: auto;
+  overflow-y: scroll;
   > .project-banner, > .project-content {
     position: absolute;
     width: inherit;

browser/src/stylesheets/style.scss
@@ -1,3 +1,4 @@
+@import "../../../node_modules/animate.css/animate.min.css";
 @import "colors";
 @import "fonts";
 @import "constants";

webpack.config.js
@@ -40,7 +40,7 @@ for (let i = 0; i < imageTypes.length; i++) {
 
 // Vendor dependencies, isolated for chunking
 const vendorDependencies = [
-  'axios', 'material-ui', 'gsap',
+  'axios', 'material-ui', 'gsap', 'animate.css',
   'react', 'react-dom', 'react-helmet', 'react-router',
   'redux', 'react-redux', 'redux-logger', 'redux-thunk', 'reselect'
 ]
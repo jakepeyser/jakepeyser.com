a628536cee3b568ab51c71f3ee1a39486e8e6be6
Jake Peyser <jakepeyser@gmail.com>
2017-01-09 22:08:40 -0500

    Subtle animations (#33)
    - basic animations
    - fixes page reload issue caused by https://github.com/jakepeyser/jakepeyser.com/commit/f8d2e8240e1ee11264d7ca61e1821c6d70b87f20

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ for (let i = 0; i < imageTypes.length; i++) {
 
 // Vendor dependencies, isolated for chunking
 const vendorDependencies = [
-  'axios', 'material-ui', 'gsap',
+  'axios', 'material-ui', 'gsap', 'animate.css',
   'react', 'react-dom', 'react-helmet', 'react-router',
   'redux', 'react-redux', 'redux-logger', 'redux-thunk', 'reselect'
 ]
