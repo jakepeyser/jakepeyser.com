0a6b0f25c54de84a53d5e6b8675b85d948b7d63c
Jake Peyser <jakepeyser@gmail.com>
2017-06-03 18:09:48 -0400

    use cache for static js files

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 // Logging, static, and body-parser middleware
 app.use(morgan('dev'));
-app.use(express.static(PATHS.build));
+app.use(express.static(PATHS.build, { maxAge: '30d' }));
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
 
