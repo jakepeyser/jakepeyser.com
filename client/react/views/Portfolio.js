9a2be6c5aca0bc23e3bafe86df890353bbe45d6c
Jake Peyser <jakepeyser@gmail.com>
2017-06-03 19:17:21 -0400

    Pre-load project banner images

--- client/react/views/Portfolio.js
+++ client/react/views/Portfolio.js
@@ -3,26 +3,54 @@ import Helmet from 'react-helmet'
 import ProjectBubble from '../components/ProjectBubble'
 import classNames from 'classnames'
 import projects from '../data/projects'
+import { getStaticResourceLink } from '../utils'
 
-export default ({ children, location }) => {
-  const fullView = location.pathname === '/projects'
-  const portfolioClass = classNames('portfolio', { 'portfolio--sm': !fullView })
-  return (
-    <div id="portfolio" className={ portfolioClass }>
-      <Helmet title="Portfolio" />
-      <div className="project-bubbles__wrapper">
-        <div className="project-bubbles__bg" />
-        <div className="project-bubbles">
-        {projects.map(project =>
-          <ProjectBubble key={ project.id }
-            name={ project.name }
-            filename={ project.filename }
-            small={ !fullView }
-          />
-        )}
+export default class Portfolio extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadedProjects: []
+    }
+    this.preloadProjectImage = this.preloadProjectImage.bind(this)
+  }
+
+  // When hovering over a project bubble, preload images to improve performance
+  preloadProjectImage({ target }) {
+    // Bubble up to link wrapper if needed
+    while (target.nodeName !== 'A') {
+      target = target.parentNode
+    }
+
+    // Load corresponding project's images if not yet loaded
+    let project = target.dataset.project
+    if (!this.state.loadedProjects.includes(project)) {
+      const projectBanner = new Image()
+      projectBanner.src = getStaticResourceLink(`images/${project}/banner.png`)
+      this.setState({ loadedProjects: [...this.state.loadedProjects, project] })
+    }
+  }
+
+  render() {
+    const fullView = this.props.location.pathname === '/projects'
+    const portfolioClass = classNames('portfolio', { 'portfolio--sm': !fullView })
+    return (
+      <div id="portfolio" className={ portfolioClass }>
+        <Helmet title="Portfolio" />
+        <div className="project-bubbles__wrapper">
+          <div className="project-bubbles__bg" />
+          <div className="project-bubbles">
+          {projects.map(project =>
+            <ProjectBubble key={ project.id }
+              name={ project.name }
+              filename={ project.filename }
+              small={ !fullView }
+              bubbleHover={ this.preloadProjectImage }
+            />
+          )}
+          </div>
         </div>
+        { this.props.children }
       </div>
-      { children }
-    </div>
-  )
+    )
+  }
 }
