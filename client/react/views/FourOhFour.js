ce244d8aabb56a8d50868727cb337d7ca4714b6a
Jake Peyser <jakepeyser@gmail.com>
2017-06-03 17:19:54 -0400

    Fixed 404 page (#43)

--- client/react/views/FourOhFour.js
+++ client/react/views/FourOhFour.js
@@ -5,12 +5,18 @@ import navList from '../components/navbar/nav-list'
 
 export default () => (
   <div id="fourohfour">
-    <div className="apology-wrapper">
-      <div className="apology">
+    <div className="fourohfour__apology-wrapper">
+      <div className="fourohfour__apology">
         <h1>404</h1>
-        <h2>Uh Oh...</h2>
-        <p className = "sub-caption">You seem to have caught me with my pants down.</p>
-        <p>I'd be oh so grateful if you would navigate to one of the main pages below. Oh, and zip me up on the way out, would you?</p>
+        <h2 className = "fourohfour__caption">Uh Oh...</h2>
+        <div className = "fourohfour__sub-caption fourohfour__sub-caption--large">
+          <p>You seem to have caught me with my pants down.</p>
+          <p>I'd be oh so grateful if you would navigate to one of the main pages below. Oh, and zip me up on the way out, would you?</p>
+        </div>
+        <div className = "fourohfour__sub-caption fourohfour__sub-caption--small">
+          <p>Well this is awkward.</p>
+          <p>How about you navigate to one of the links below and we pretend this little mishap never happened?</p>
+        </div>
         <ul>
         {
           navList.map(link => {
@@ -28,7 +34,7 @@ export default () => (
         </ul>
       </div>
     </div>
-    <div className="zipper-wrapper">
+    <div className = "fourohfour__zipper-wrapper">
       <Zipper
         numTeeth={ 26 }
         message="404"
