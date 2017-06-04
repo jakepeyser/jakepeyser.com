a93ab92a61f0ae2d6edd2b2e2bc0a07e24a66f31
Jake Peyser <jakepeyser@gmail.com>
2017-06-04 13:13:22 -0400

    Load optimization (#44)
    * CSS modules and minification
    
    * use cache for static js files
    
    * Pre-load project banner images

--- client/react/components/ProjectBubble.js
+++ client/react/components/ProjectBubble.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Link } from 'react-router'
 import classNames from 'classnames'
 
-export default ({ name, filename, small }) => {
+export default ({ name, filename, small, bubbleHover }) => {
   const bubbleClass = classNames('project-bubble', { 'project-bubble--sm': small })
   const bubbleIconClass = classNames('project-bubble__icon', `i-${filename}`)
   const bubbleTextClass = classNames('project-bubble__text', { 'project-bubble__text--sm': small })
   return (
     <Link to={`/projects/${filename}`}
       className={ bubbleClass }
+      data-project={ filename }
       activeClassName="project-bubble--active"
+      onMouseEnter={ bubbleHover }
       onClick={ () => {
         const projContainer = document.querySelector('#project');
         if (projContainer) projContainer.scrollTop = 0;
