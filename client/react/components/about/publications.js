1076339e24ab1bbed382722c2f01742d9b6f0a91
Jake Peyser <jakepeyser@gmail.com>
2017-09-30 12:37:44 -0400

    added Vue->MobX blog to publications

--- client/react/components/about/publications.js
+++ client/react/components/about/publications.js
@@ -1,4 +1,5 @@
 export default [
+  { title: 'The Journey from Vue to MobX', link: 'https://appendto.com/2017/08/journey-vue-mobx/', date: 1503662400 },
   { title: 'Intro to Hoodie and React', link: 'https://css-tricks.com/intro-hoodie-react/', date: 1500465600 },
   { title: 'Where Did Async/Await Come from and Why Use It?', link: 'https://appendto.com/2017/06/asyncawait-come-use/', date: 1497441600 },
   { title: 'Building a Blog with Ghost and Heroku', link: 'http://blog.jakepeyser.com/making-the-blog/', date: 1486468800 },
