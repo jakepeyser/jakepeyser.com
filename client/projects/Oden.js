4266ef3f309c3c47895543650af8bd79701fbdb2
Jake Peyser <jakepeyser@gmail.com>
2020-03-22 23:24:07 -0400

    Jp/mar 2020 (#58)
    * update footer date
    
    * added Oden page

---
+++ client/projects/Oden.js
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+export default {
+  name: 'Oden Technologies',
+  filename: 'oden',
+  links: [
+    { url: 'https://oden.io', type: 'desktop' }
+  ],
+  summary: {
+    Company: 'Oden Technologies',
+    Description: 'Industrial IoT & analytics for manufacturers',
+    Date: 'Ongoing',
+    Responsibility: 'Lead the product engineering team consisting of backend and frontend engineers. As the fourth engineer, I have helped design and implement essential pieces of the platform including configurable dashboards, real-time alerting, and our hybrid cloud solutions.',
+    Technology: ['React', 'MobX', 'Node.js', 'GraphQL', 'Golang', 'Python', 'GCP (BigTable, Kubernetes, GCS, Cloud Build)', 'Airflow', 'Heroic', 'PostgresDB', 'Kubernetes']
+  },
+  background: 'Oden uses machine learning and applied analytics to provide manufacturers with the most efficient way to make any product. Our team builds and maintains all of our internal and external web applications, REST & GraphQL APIs, multiple storage layers and key data pipeline services. We work closely with the product and data science teams to a deliver an end-to-end solution that solves our users\' problems.',
+  images: [
+    { file: 'asset_nav_list.png', desc: 'Remotely monitor assets and get real-time updates on work order production, machine status, and other KPIs' },
+    { file: 'now_tile.png', desc: 'Stream live process data and warn users when key metrics are able to violate preset thresholds' },
+    { file: 'dashboards.png', desc: 'Build configurable dashboards for dynamic range queries that display KPIs across any dimension' },
+    { file: 'discover.png', desc: 'Pivot aggregate data on key dimensions to find biggest opportunities for eliminating waste' },
+    { file: 'explore_search.png', desc: 'Create and save metadata queries and join results against process and offline QA data' },
+    { file: 'explore_timeseries.png', desc: 'View historical time series data to quickly identify and root cause issues' },
+    { file: 'report.png', desc: 'Configure and receive daily reports to help engineers spot and analyze potential issues with yesterday\'s production' },
+    [
+      { file: 'labs.png', desc: 'View machine learning reports run against months of historical data in order to find opportunities for improved production' },
+      { file: 'operator.png', desc: 'Give operators visibility and control over the data that is ultimately used to make operational decisions' },
+    ],
+    { file: 'alert.png', desc: 'Deliver real-time alerts about errant process conditions, machine states, and more' },
+  ]
+}
+
